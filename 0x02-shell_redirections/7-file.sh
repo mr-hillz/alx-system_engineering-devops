32d184c9f560079ebaef2defbd1a1c8db7c13f7a
mr-hillz <bemtyoban@gmail.com>
2023-08-09 06:58:32 -0700

    seven file

--- 0x02-shell_redirections/7-file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename="*\\\\'\"Best School\"'\\\\*\\$\\?******:)"
-content="Best School\n"
-
-echo -e "$content" > "$filename"
-echo "File '$filename' created with content:"
-cat "$filename"
-
